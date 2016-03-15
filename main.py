01f64171ff2e84a735578132f35a41a678381c35
namickey <teethdai@gmail.com>
2016-03-15 22:09:51 +0900

    update

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
--"""`main` is the top level module for your Flask application."""
+"""`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, redirect, url_for
 from google.appengine.ext import ndb
 
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-class Greeting(ndb.Model):
-    content = ndb.StringProperty()
+class Sensor(ndb.Model):
+    value = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
-    
+
     @classmethod
-    def query_book(cls, ancestor_key):
+    def query_sensor(cls, ancestor_key):
         return cls.query(ancestor=ancestor_key).order(-cls.date)
 
-@app.route('/')
-def hello():
-    greets = Greeting.query()
+@app.route('/tem')
+def show():
+    cate = 'tem'
+    ancestor_key = ndb.Key("Sensor", cate)
+    sensors = Sensor.query_sensor(ancestor_key).fetch(20)
     s = ''
-    for greet in greeths:
-        s = s + str(greet) + '\n'
+    for sensor in sensors:
+        s = s + sensor.value + ',' + str(sensor.date) + '\n'
     return 'Hello World! gae!\n' + s
 
-@app.route('/hi/<user>')
-def hi(user):
-    greeting = Greeting(parent=ndb.Key("Book",user),content=user)
-    greeting.put()
-    return 'hi ' + user + ' !!!'
+@app.route('/<cate>/<value>')
+def regist(cate, value):
+    sensor = Sensor(parent=ndb.Key("Sensor",cate),value=value)
+    sensor.put()
+    return redirect(url_for('show') + "/" + cate)
 
 @app.errorhandler(404)
 def page_not_found(e):
